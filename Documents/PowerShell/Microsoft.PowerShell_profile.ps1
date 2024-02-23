c61926f6971d5da786d414b244dd21ed54046e09
Iain Galloway <iain.galloway@gmail.com>
2024-02-23 12:38:17 +0000

    Change Windows Terminal to light mode theme

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,29 @@
 Set-PSReadLineOption -EditMode Emacs
+
+Set-PSReadLineOption -Colors @{
+  Command            = 'Black'
+  Number             = 'DarkGray'
+  Member             = 'DarkGray'
+  Operator           = 'DarkGray'
+  Type               = 'DarkGray'
+  Variable           = 'DarkGreen'
+  Parameter          = 'DarkGreen'
+  ContinuationPrompt = 'DarkGray'
+  Default            = 'DarkGray'
+}
+
+$p = $host.privatedata
+$p.ErrorForegroundColor    = "Red"
+$p.ErrorBackgroundColor    = "White"
+$p.WarningForegroundColor  = "Yellow"
+$p.WarningBackgroundColor  = "White"
+$p.DebugForegroundColor    = "Yellow"
+$p.DebugBackgroundColor    = "White"
+$p.VerboseForegroundColor  = "Black"
+$p.VerboseBackgroundColor  = "White"
+$p.ProgressForegroundColor = "DarkGray"
+$p.ProgressBackgroundColor = "White"
+
 Invoke-Expression (&starship init powershell)
 
 # chezmoi
